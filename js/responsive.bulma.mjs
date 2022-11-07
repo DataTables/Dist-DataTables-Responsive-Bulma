30f54d7502b2a5d76b67e66ffecac8ad39968546
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-07 16:39:04 +0000

    89b36ae8eb924baa2ef7aa515684764a7edbfc12 Release 2.4.0
    Sync to source repo @89b36ae8eb924baa2ef7aa515684764a7edbfc12

--- js/responsive.bulma.mjs
+++ js/responsive.bulma.mjs
@@ -1,11 +1,10 @@
-
 /*! Bulma integration for DataTables' Responsive
  * © SpryMedia Ltd - datatables.net/license
  */
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-bm';
-import DataTable from 'datatables.net-responsive';
+import 'datatables.net-responsive';
 
 
 
